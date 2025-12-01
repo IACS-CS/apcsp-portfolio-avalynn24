445d96d7494b82f26d8118a778c838672932d010
avalynn24 <avalynn.annarelli@innovationcharter.org>
2025-12-01 14:28:48 +0000

    made new page for canvasgame(not done yet)

---
+++ src/scripts/pages/canvasgame.js
@@ -0,0 +1,26 @@
+// src/scripts/pages/binarySearch.js
+import { renderMenu } from "../utils/renderMenu.js";
+import { renderIframe } from "../utils/renderIframe.js";
+
+// Render the Binary Search project page
+export function renderBinarySearch() {
+  /* html */
+  return `
+    <!-- code generated in binarySearch.js -->
+    <h1 class="animate__animated animate__bounceInDown">Binary Search Project</h1>    
+    ${renderMenu("#/binary-search")}
+  
+    <article>
+      <p>Assignment: Create a program built on the concept of binary search</p>
+      <hr>
+      <p>In this project Chase and I decided to create a binary search game where 
+      the user has to think of a linix distro and the program has to guess what distro 
+      they're thinking of using different queations.</p>
+      ${renderIframe(
+        "https://iacs-cs.github.io/simple-canvas-game-dvd-consortium/",
+        "My Project Title"
+      )}
+    </article>
+    <!-- end code generated in binarySearch.js -->
+  `;
+}
